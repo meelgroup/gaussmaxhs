5b87eb091b0c87d03dd080d18e8fa0c1ddf1af9a
Mate Soos <soos.mate@gmail.com>
2018-04-06 18:38:11 +0800

    Just some help into fuzz generator

--- build/release/bin/fuzz.sh
+++ build/release/bin/fuzz.sh
@@ -10,7 +10,10 @@ echo ./maxhs in_wcnf_xor_blasted
 cat in_wcnf_xor_blasted | ./strip_wcnf.py -s strip_wcnf.py > in_cnf_xors
 echo ~/development/sat_solvers/cryptominisat/build/cryptominisat5 --maxmatrixrows 100000 in_cnf_xors
 cat in_wcnf_xor_blasted | ./strip_wcnf.py -s -x strip_wcnf.py > in_cnf_blastedxors
-echo ./maxhs_old in_cnf_blastedxors
+
+# strip xor in_wcnf_xor_blasted
+cat in_wcnf_xor_blasted | ././strip_wcnf.py -x > in_wcnf_xor_blasted_nox
+echo ./maxhs_old in_wcnf_xor_blasted_nox
 
 echo "diff -y  in_cnf in_wcnf_xor  | colordiff | less -R"
 echo "diff -y  in_wcnf_xor in_wcnf_xor_blasted | colordiff | less -R"
