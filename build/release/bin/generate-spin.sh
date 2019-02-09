f58640eea5760d0f008f825b4e330f5a1d17ba44
Mate Soos <soos.mate@gmail.com>
2019-02-09 21:17:32 +0100

    Better indentation

--- build/release/bin/generate-spin.sh
+++ build/release/bin/generate-spin.sh
@@ -24,25 +24,25 @@ cat ${preset}_wcnf_xor_blasted | ./strip_wcnf.py -x > "${preset}_wcnf_xor_blaste
 
 echo "running $f"
 (
-rm -f "${preset}.outorig"
-ulimit -t $tlimit
-ulimit -v $memlimit
-/usr/bin/time --verbose -o "${preset}.timeoutorig" ./maxhs_orig -verb=0 "${preset}_wcnf_xor_blasted_nox" > "${preset}.outorig"
-grep UNSAT "${preset}.outorig" || true
-orig=$(grep "^o " "${preset}.outorig")
-echo "$orig"
+	rm -f "${preset}.outorig"
+	ulimit -t $tlimit
+	ulimit -v $memlimit
+	/usr/bin/time --verbose -o "${preset}.timeoutorig" ./maxhs_orig -verb=0 "${preset}_wcnf_xor_blasted_nox" > "${preset}.outorig"
+	grep UNSAT "${preset}.outorig" || true
+	orig=$(grep "^o " "${preset}.outorig")
+	echo "$orig"
 )
 orig=$(grep "^o " "${preset}.outorig")
 origtime=$(grep "User time" "${preset}.timeoutorig" | cut -d " " -f 4)
 
 (
-rm -f "${preset}.outnew"
-ulimit -t $tlimit
-ulimit -v $memlimit
-/usr/bin/time --verbose -o "${preset}.timeoutnew" ./maxhs -verb=0  "${preset}_wcnf_xor_blasted" > "${preset}.outnew"
-grep UNSAT "${preset}.outnew" || true
-new=$(grep "^o " "${preset}.outnew")
-echo "$new"
+	rm -f "${preset}.outnew"
+	ulimit -t $tlimit
+	ulimit -v $memlimit
+	/usr/bin/time --verbose -o "${preset}.timeoutnew" ./maxhs -verb=0  "${preset}_wcnf_xor_blasted" > "${preset}.outnew"
+	grep UNSAT "${preset}.outnew" || true
+	new=$(grep "^o " "${preset}.outnew")
+	echo "$new"
 )
 new=$(grep "^o " "${preset}.outnew")
 newtime=$(grep "User time" "${preset}.timeoutnew" | cut -d " " -f 4)
