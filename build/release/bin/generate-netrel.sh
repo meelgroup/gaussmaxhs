1a72dbb74ded437239cf4ac3ac08a19a88d20332
Mate Soos <soos.mate@gmail.com>
2019-02-10 23:23:11 +0100

    Updating gen-netrel

--- build/release/bin/generate-netrel.sh
+++ build/release/bin/generate-netrel.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-set -e
 set -x
 
-numxors=$1
-tlimit=$2
-memlimit=$3
-if [[ $numxors == "" ]]; then
-	echo "ERROR You must give number of XORs as 1st argument"
-	exit -1
-fi
+function compare_the_two
+{
+    echo "running $f"
+    (
+        rm -f "${preset}.outorig"
+        ulimit -t "$tlimit"
+        ulimit -v "$memlimit"
+        /usr/bin/time --verbose -o "${preset}.timeoutorig" ./maxhs_orig -verb=0 "${preset}_wcnf_xor_blasted_nox" > "${preset}.outorig"
+        grep UNSAT "${preset}.outorig" || true
+        orig=$(grep "^o " "${preset}.outorig")
+        echo "$orig"
+    )
+    orig=$(grep "^o " "${preset}.outorig")
+    origtime=$(grep "User time" "${preset}.timeoutorig" | cut -d " " -f 4)
+
+    (
+        rm -f "${preset}.outnew"
+        ulimit -t "$tlimit"
+        ulimit -v "$memlimit"
+        /usr/bin/time --verbose -o "${preset}.timeoutnew" ./maxhs -verb=0  "${preset}_wcnf_xor_blasted" > "${preset}.outnew"
+        grep UNSAT "${preset}.outnew" || true
+        new=$(grep "^o " "${preset}.outnew")
+        echo "$new"
+    )
+    new=$(grep "^o " "${preset}.outnew")
+    newtime=$(grep "User time" "${preset}.timeoutnew" | cut -d " " -f 4)
+
+
+    echo "orig vs new time: $origtime --- $newtime"
+    totalorig=$(echo "$totalorig + $origtime" | bc)
+    totalnew=$(echo "$totalnew + $newtime" | bc)
+}
+
+tlimit=$1
+memlimit=$2
 
 if [[ $tlimit == "" ]]; then
 	echo "ERROR You must give timeout as 2nd argument"
@@ -20,60 +47,45 @@ if [[ $memlimit == "" ]]; then
 	echo "ERROR You must give mem limit in kb as 3rd argument"
 	exit -1
 fi
-echo "xors: ${numxors}"
 echo "tlimit: ${tlimit}  memlimit ${memlimit}"
+
 
 totalorig=0
 totalnew=0
+ls network-reliability/*.cnf > mylist
+shuf mylist | head -n 1 > mylist2
 
-cp network-reliability/Net22_75_10_count_116.cnf .
-f="Net22_75_10_count_116.cnf"
-preset="mystuff"
-rm -f "${preset}-orig"
-mv ${f} "${preset}-orig"
-echo "----------------"
-echo "Converting $f"
-cat "${preset}-orig" | ./xor_to_cnf.py > "${preset}_wcnf_xor_blasted"
+for f in `cat mylist2` ;do
+    for numxors in {0..2} ;do
+        f=$(basename "$f")
+        cp "network-reliability/$f" .
+        ./add_xors.py -f "$f" -o "$f-withxors" --xors $numxors
 
-# strip xor for orig maxhs
-cat "${preset}_wcnf_xor_blasted" | ./strip_wcnf.py -x > "${preset}_wcnf_xor_blasted_nox"
+        f="$f-withxors"
+        preset="$f"
+        rm -f "${preset}-orig"
+        mv ${f} "${preset}-orig"
+        echo "----------------"
+        echo "Converting $f"
+        cat "${preset}-orig" | ./xor_to_cnf.py > "${preset}_wcnf_xor_blasted"
 
-echo "running $f"
-(
-	rm -f "${preset}.outorig"
-	ulimit -t "$tlimit"
-	ulimit -v "$memlimit"
-	/usr/bin/time --verbose -o "${preset}.timeoutorig" ./maxhs_orig -verb=0 "${preset}_wcnf_xor_blasted_nox" > "${preset}.outorig"
-	grep UNSAT "${preset}.outorig" || true
-	orig=$(grep "^o " "${preset}.outorig")
-	echo "$orig"
-)
-orig=$(grep "^o " "${preset}.outorig")
-origtime=$(grep "User time" "${preset}.timeoutorig" | cut -d " " -f 4)
+        # strip xor for orig maxhs
+        cat "${preset}_wcnf_xor_blasted" | ./strip_wcnf.py -x > "${preset}_wcnf_xor_blasted_nox"
 
-(
-	rm -f "${preset}.outnew"
-	ulimit -t "$tlimit"
-	ulimit -v "$memlimit"
-	/usr/bin/time --verbose -o "${preset}.timeoutnew" ./maxhs -verb=0  "${preset}_wcnf_xor_blasted" > "${preset}.outnew"
-	grep UNSAT "${preset}.outnew" || true
-	new=$(grep "^o " "${preset}.outnew")
-	echo "$new"
-)
-new=$(grep "^o " "${preset}.outnew")
-newtime=$(grep "User time" "${preset}.timeoutnew" | cut -d " " -f 4)
+        compare_the_two
 
+        rm "${preset}.outnew"
+        rm "${preset}.outorig"
+        rm "${preset}.timeoutorig"
+        rm "${preset}.timeoutnew"
 
-echo "orig vs new time: $origtime --- $newtime"
-totalorig=$(echo "$totalorig + $origtime" | bc)
-totalnew=$(echo "$totalnew + $newtime" | bc)
+        if [ "$orig" == "$new" ]; then
+            echo "OK: $orig -- $new"
+        else
+            echo "ERRROR! Not the same result!"
+        fi
+    done
+done
 
-rm ${preset}_wcnf*
-
-if [ "$orig" == "$new" ]; then
-    echo "OK: $orig -- $new"
-else
-    echo "ERRROR! Not the same result!"
-fi
 echo "Total orig: $totalorig"
 echo "Total new : $totalnew"
