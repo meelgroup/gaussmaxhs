84e79f1e697fe86201071202463790c2615986fe
Mate Soos <soos.mate@gmail.com>
2021-10-26 20:11:51 +0200

    Updating copyright header

--- minisat/core/gaussian.h
+++ minisat/core/gaussian.h
@@ -1,5 +1,5 @@
 /******************************************
-Copyright (c) 2016, Mate Soos
+Copyright (c) 2016-2021, Mate Soos
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
